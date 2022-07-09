24f7bc3299162375e8bd3a0ccad66733b300ce44
Maysafig <maysafig@gmail.com>
2022-07-08 21:19:03 -0300

    Automação de testes - Aula quarta - BDD

--- projeto1/test/vendas.test.js
+++ projeto1/test/vendas.test.js
@@ -1,5 +1,7 @@
 const somaCompras = require("../testes-unitarios/compras.js");
 const estoque = require("../testes-unitarios/estoque.js");
+const carrinho = require("../testes-unitarios/livraria.js");
+
 
 test("Deve retornar a soma das compras com taxa de entrega", () => {
     const esperado = 2530;
@@ -11,6 +13,14 @@ test("Deve retornar a soma das compras com taxa de entrega", () => {
 test("Deve retornar a diferença que restou no estoque", () => {
     const esperado = 280;
     const retornado = estoque(310, 30);
+
+    expect(retornado).toBe(esperado)
+});
+
+test("Deve retornar a quantidade total do carrinho", () => {
+    const esperado = 2;
+    const retornado = carrinho(2, 0);
   
     expect(retornado).toBe(esperado)
-  });
+});
+  
